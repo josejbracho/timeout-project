8ba41c449db84142dc8d181e081c5fdfad74922a
jbracho <josejbracho@gmail.com>
2021-08-17 18:33:06 -0400

    First commit

---
+++ problems/04-interval-count.js
@@ -0,0 +1,34 @@
+/***********************************************************************
+Write a function `intervalCount` that accepts a callback, a delay in 
+milliseconds, and an amount. The function should set an interval with 
+the given callback and delay, but clear the interval after the callback
+has been executed 'amount' number of times.
+
+Hint: utilize a 'closure' to your advantage
+
+In addition to Mocha, we recommend that you test your code manually using 
+node with the examples below.
+
+Example
+
+intervalCount(function() {
+    console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+***********************************************************************/
+
+function intervalCount(cb, delay, n) {
+  const TimeoutObj = setInterval(function () {
+    cb();
+    n--
+    if (n === 0) {
+      clearInterval(TimeoutObj)
+    }
+  }, delay)
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = intervalCount;
+} catch {
+  module.exports = null;
+}
